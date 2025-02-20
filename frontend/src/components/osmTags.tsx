09cfb254c996f30576d2dbbdf31b205e16ec6f69
Johannes Skivdal <johannes@skivdaldata.no>
2025-02-20 13:08:11 +0100

    Removes survey QR

--- frontend/src/components/osmTags.tsx
+++ frontend/src/components/osmTags.tsx
@@ -3,13 +3,6 @@ import useBackend, { SourceResponse } from "@/useBackend";
 export default function OsmTags({ videoId }: { videoId: string | undefined }) {
   const data = useBackend<SourceResponse>(videoId, "osmtags");
 
-  return (
-    <div>
-      Use the survey: <a href="https://forms.gle/zgX2bihHTo5NGdN38" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">https://forms.gle/zgX2bihHTo5NGdN38</a> <br />
-
-      Or scan the code:
-      <img style={{ maxHeight: "200px", }} src="/survey-qr.jpg"></img>
-    </div>
-  );
+  return <div></div>;
 }
 
