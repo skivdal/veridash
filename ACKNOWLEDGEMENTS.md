fc0e9f2cfd500b638ad84878f3d2f117829f3e4b
Johannes Skivdal <johannes@skivdaldata.no>
2025-09-24 09:39:14 +0200

    Add acknowledgements

---
+++ ACKNOWLEDGEMENTS.md
@@ -0,0 +1,20 @@
+# Acknowledgements
+
+This project makes use of the following open-source software and libraries:
+
+- [MinIO](https://min.io/)
+- [PostgreSQL 16](https://www.postgresql.org/)
+
+*Backend*
+Python 3.12, FastAPI, Uvicorn, Celery, minio-py, websockets, psycopg, Redis, ffmpeg-python, Werkzeug, OpenAI Whisper, OpenAI Python Client, Ultralytics, OpenCV
+
+*Frontend*
+Next.js, React, React DOM, Tailwind CSS, Headless UI, Heroicons, Leaflet, React Leaflet, leaflet-defaulticon-compatibility, iso-639-1, react-json-to-table, react-use-websocket
+
+*Development Tools*
+TypeScript, ESLint, eslint-config-next, PostCSS, @types/*
+
+---
+
+Thanks to the developers and contributors of these projects for making this work possible.
+
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
@@ -21,15 +22,15 @@ export default function Home() {
 
         {/* Logo and tagline */}
 
-        <div className=" relative flex flex-col items-center mb-1">
-        <h1 className="text-5xl sm:text-6xl font-bold flex items-center gap-1">
-            <img src="/temp_veri_logo.png" alt="Logo" width={90} height={90} />
-            <span className="text-black">eriDash</span>
-        </h1>
-
-        <p className="desc">
+        <div className="relative flex flex-col items-center mb-1">
+          <h1 className="text-5xl sm:text-6xl font-bold flex items-center gap-1">
+              <img src="/temp_veri_logo.png" alt="Logo" width={90} height={90} />
+              <span className="text-black">eriDash</span>
+          </h1>
+  
+          <p className="desc">
             Your Verification Command Center
-            </p>
+          </p>
         </div>
 
 
@@ -39,13 +40,14 @@ export default function Home() {
             </p>
 
 
-        {/* key control instructions */}
+        {/* key control instructions 
         <div className="flex flex-col gap-4 text-left mb-10 w-full max-w-md">
           <Instruction keyLabel="Z" text="To resize the prototype window" />
           <Instruction keyLabel="R" text="To restart the flow" />
           <Instruction keyLabel="← / →" text="To skip frames" />
         </div>
-
+        */}
+        
         {/* begin Button */}
         <button
           onClick={() => router.push("/dashboard")}
@@ -53,6 +55,16 @@ export default function Home() {
         >
           Begin
         </button>
+        
+        <div className="mt-10">
+          <p className="text-gray-400 text-left mb-5">
+            The development of VeriDash back-end, specifically the job scheduler, database design, file transfer, and modules for keyframes, stitching, transcription, metadata, and map, is funded by European Union under Action 101158604 — NORDIS, funded through the Digital Europe Programme (DIGITAL) under the DIGITAL-2023-DEPLOY-04-EDMO-HUBS call (Digital SME Support Actions), managed by the European Health and Digital Executive Agency (HaDEA).
+          </p>
+          
+          <Image src="/nordis-logo.png" alt="NORDIS logo" width={363} height={72} className="inline mr-5"></Image>
+          <Image src="/funded-by-eu.jpeg" alt="NORDIS logo" width={88} height={88} className="inline"></Image>
+        </div>
+        
       </div>
     </main>
   );
