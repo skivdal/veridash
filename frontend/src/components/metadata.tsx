53919a69c2f4466b9d2940204df75b94b4c798c3
Johannes Skivdal <johannes@skivdaldata.no>
2025-12-19 13:03:03 +0100

    fix: temp. remove dependency react-json-table

--- frontend/src/components/metadata.tsx
+++ frontend/src/components/metadata.tsx
@@ -1,15 +1,16 @@
-import { JsonToTable } from "react-json-to-table";
+// import { JsonToTable } from "react-json-to-table";
 import useBackend, { MetadataResponse } from "@/useBackend";
 
 export default function Metadata({ videoId }: { videoId: string | undefined }) {
   const data = useBackend<MetadataResponse>(videoId, "metadata");
 
+  /* <JsonToTable json={(data as MetadataResponse).format} /> */
   return (
     <div className="h-full overflow-auto hover:overflow-scroll">
       <p className="mb-2">Metadata</p>
       <div className="text-xs">
         {data && (data as MetadataResponse)?.format ?
-          <JsonToTable json={(data as MetadataResponse).format} />
+          <pre>{ JSON.stringify(data, undefined, 2) }</pre>
           : ''}
       </div>
     </div>
